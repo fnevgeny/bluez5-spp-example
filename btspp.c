0658bbf4b87898a266913fc9f84fc2c796b0f918
Evgeny Stambulchik <fnevgeny@gmail.com>
2020-07-18 14:18:52 +0300

    Replace hardware tabs

--- btspp.c
+++ btspp.c
@@ -33,234 +33,234 @@
 #define SERIAL_PORT_PROFILE_UUID "00001101-0000-1000-8000-00805f9b34fb"
 
 typedef struct {
-	uint8_t b[6];
+    uint8_t b[6];
 } __attribute__((packed)) bdaddr_t;
 
 struct sockaddr_rc {
-	sa_family_t rc_family;
-	bdaddr_t    rc_bdaddr;
-	uint8_t     rc_channel;
+    sa_family_t rc_family;
+    bdaddr_t    rc_bdaddr;
+    uint8_t     rc_channel;
 };
 
 struct spp_data {
-	GMainLoop *loop;	
-	int sock_fd;
-	struct sockaddr_rc local;
-	struct sockaddr_rc remote;	
+    GMainLoop *loop;
+    int sock_fd;
+    struct sockaddr_rc local;
+    struct sockaddr_rc remote;
 };
 
 int register_profile(struct spp_data *spp, GDBusProxy *proxy)
 {
-	GVariant *profile;
-	GVariantBuilder profile_builder;
-	GError *error = NULL;
+    GVariant *profile;
+    GVariantBuilder profile_builder;
+    GError *error = NULL;
 
-	printf("register_profile called!\n");
-	
-	g_variant_builder_init(&profile_builder, G_VARIANT_TYPE("(osa{sv})"));
+    printf("register_profile called!\n");
 
-	if (g_variant_is_object_path("/bluetooth/profile/serial_port")) {
-		printf("object path is good!\n");
-	}
-	
-	g_variant_builder_add (&profile_builder, "o",
-			"/bluetooth/profile/serial_port");
+    g_variant_builder_init(&profile_builder, G_VARIANT_TYPE("(osa{sv})"));
 
-	g_variant_builder_add (&profile_builder, "s", SERIAL_PORT_PROFILE_UUID);
+    if (g_variant_is_object_path("/bluetooth/profile/serial_port")) {
+        printf("object path is good!\n");
+    }
 
-	g_variant_builder_open(&profile_builder, G_VARIANT_TYPE("a{sv}"));
+    g_variant_builder_add (&profile_builder, "o",
+            "/bluetooth/profile/serial_port");
 
-        // not specifying AutoConnect...
-	
-	g_variant_builder_open(&profile_builder, G_VARIANT_TYPE("{sv}"));	
-	g_variant_builder_add (&profile_builder, "s", "Channel");
-	g_variant_builder_add (&profile_builder, "v", g_variant_new_uint16(22));
-	g_variant_builder_close(&profile_builder);
+    g_variant_builder_add (&profile_builder, "s", SERIAL_PORT_PROFILE_UUID);
 
-	g_variant_builder_open(&profile_builder, G_VARIANT_TYPE("{sv}"));
-	g_variant_builder_add (&profile_builder, "s", "Service");
-	g_variant_builder_add (&profile_builder, "v",
-			g_variant_new_string(SERIAL_PORT_PROFILE_UUID));
-	g_variant_builder_close(&profile_builder);
+    g_variant_builder_open(&profile_builder, G_VARIANT_TYPE("a{sv}"));
 
-	g_variant_builder_open(&profile_builder, G_VARIANT_TYPE("{sv}"));
-	g_variant_builder_add (&profile_builder, "s", "Name");
-	g_variant_builder_add (&profile_builder, "v",
-			g_variant_new_string("Serial Port"));
-	g_variant_builder_close(&profile_builder);
+    // not specifying AutoConnect...
 
-	g_variant_builder_open(&profile_builder, G_VARIANT_TYPE("{sv}"));
-	g_variant_builder_add (&profile_builder, "s", "Role");
+    g_variant_builder_open(&profile_builder, G_VARIANT_TYPE("{sv}"));
+    g_variant_builder_add (&profile_builder, "s", "Channel");
+    g_variant_builder_add (&profile_builder, "v", g_variant_new_uint16(22));
+    g_variant_builder_close(&profile_builder);
 
-	g_variant_builder_add (&profile_builder, "v",
-				g_variant_new_string("server"));
-		
-	g_variant_builder_close(&profile_builder);
+    g_variant_builder_open(&profile_builder, G_VARIANT_TYPE("{sv}"));
+    g_variant_builder_add (&profile_builder, "s", "Service");
+    g_variant_builder_add (&profile_builder, "v",
+            g_variant_new_string(SERIAL_PORT_PROFILE_UUID));
+    g_variant_builder_close(&profile_builder);
+
+    g_variant_builder_open(&profile_builder, G_VARIANT_TYPE("{sv}"));
+    g_variant_builder_add (&profile_builder, "s", "Name");
+    g_variant_builder_add (&profile_builder, "v",
+            g_variant_new_string("Serial Port"));
+    g_variant_builder_close(&profile_builder);
+
+    g_variant_builder_open(&profile_builder, G_VARIANT_TYPE("{sv}"));
+    g_variant_builder_add (&profile_builder, "s", "Role");
+
+    g_variant_builder_add (&profile_builder, "v",
+                g_variant_new_string("server"));
+
+    g_variant_builder_close(&profile_builder);
 
 
-	g_variant_builder_close(&profile_builder);
-	profile = g_variant_builder_end(&profile_builder);
+    g_variant_builder_close(&profile_builder);
+    profile = g_variant_builder_end(&profile_builder);
 
-	g_dbus_proxy_call_sync (proxy,
-				"RegisterProfile",
-				profile,
-				G_DBUS_CALL_FLAGS_NONE,
-				-1,
-				NULL,
-				&error);
-	g_assert_no_error(error);
+    g_dbus_proxy_call_sync (proxy,
+                "RegisterProfile",
+                profile,
+                G_DBUS_CALL_FLAGS_NONE,
+                -1,
+                NULL,
+                &error);
+    g_assert_no_error(error);
 
-	return 0;
+    return 0;
 }
 
 gboolean
 server_read_data (gpointer user_data) {
-	char buf[1024] = { 0 };
-	int bytes_read;
-	int opts = 0;
-	struct spp_data *spp = user_data;
-	
-	printf("server_read_data called\n");
+    char buf[1024] = { 0 };
+    int bytes_read;
+    int opts = 0;
+    struct spp_data *spp = user_data;
 
-	// set socket for blocking IO
-	fcntl(spp->sock_fd, F_SETFL, opts);
-	opts = fcntl(spp->sock_fd, F_GETFL);
-	if (opts < 0) {
-		perror("fcntl(F_GETFL)");
-		exit(EXIT_FAILURE);
-	}
+    printf("server_read_data called\n");
 
-	opts &= ~O_NONBLOCK;
-	
-	if (fcntl(spp->sock_fd, F_SETFL,opts) < 0) {
-		perror("fcntl(F_SETFL)");
-		exit(EXIT_FAILURE);
-	}
+    // set socket for blocking IO
+    fcntl(spp->sock_fd, F_SETFL, opts);
+    opts = fcntl(spp->sock_fd, F_GETFL);
+    if (opts < 0) {
+        perror("fcntl(F_GETFL)");
+        exit(EXIT_FAILURE);
+    }
 
-	// read data from the client
-	bytes_read = read(spp->sock_fd, buf, sizeof(buf));
-	if ( bytes_read > 0 ) {
-                while (buf[bytes_read - 1] == '\n' ||
-                       buf[bytes_read - 1] == '\r') {
-                    buf[--bytes_read] = '\0';
-                }
-		printf("received [%s]\n", buf);
+    opts &= ~O_NONBLOCK;
 
-	        // continue listening
-	        return true;
-	} else {
-		printf("error reading from client [%d] %s\n", errno, strerror(errno));
+    if (fcntl(spp->sock_fd, F_SETFL,opts) < 0) {
+        perror("fcntl(F_SETFL)");
+        exit(EXIT_FAILURE);
+    }
 
-	        return false;
-	}
+    // read data from the client
+    bytes_read = read(spp->sock_fd, buf, sizeof(buf));
+    if ( bytes_read > 0 ) {
+        while (buf[bytes_read - 1] == '\n' ||
+               buf[bytes_read - 1] == '\r') {
+            buf[--bytes_read] = '\0';
+        }
+        printf("received [%s]\n", buf);
+
+        // continue listening
+        return true;
+    } else {
+        printf("error reading from client [%d] %s\n", errno, strerror(errno));
+
+        return false;
+    }
 }
 
 void
 print_bdaddr (gchar *prefix, const bdaddr_t *bdaddr)
 {
-	printf ("%s: ", prefix);
+    printf ("%s: ", prefix);
 
-	// print BTADDR in reverse
-	for (int i = 5; i > -1; i--) {
-		printf("%02X", bdaddr->b[i]);
-	}
+    // print BTADDR in reverse
+    for (int i = 5; i > -1; i--) {
+        printf("%02X", bdaddr->b[i]);
+    }
 
-	printf ("\n");
+    printf ("\n");
 }
 
 static gboolean
 on_handle_new_connection (OrgBluezProfile1 *interface,
-			GDBusMethodInvocation *invocation,
-			const gchar           *device,
-			const GVariant        *fd,
-			const GVariant        *fd_props,
-			gpointer              user_data)
+            GDBusMethodInvocation *invocation,
+            const gchar           *device,
+            const GVariant        *fd,
+            const GVariant        *fd_props,
+            gpointer              user_data)
 {
-	GDBusMessage *message;
-	GError *error = NULL;	
-	GUnixFDList *fd_list;
-	static socklen_t optlen = sizeof(struct sockaddr_rc);
-	struct spp_data *spp = user_data;
+    GDBusMessage *message;
+    GError *error = NULL;
+    GUnixFDList *fd_list;
+    static socklen_t optlen = sizeof(struct sockaddr_rc);
+    struct spp_data *spp = user_data;
 
-	message = g_dbus_method_invocation_get_message (invocation);
-	fd_list = g_dbus_message_get_unix_fd_list (message);
-	spp->sock_fd = g_unix_fd_list_get (fd_list, 0, &error);
-	g_assert_no_error (error);
+    message = g_dbus_method_invocation_get_message (invocation);
+    fd_list = g_dbus_message_get_unix_fd_list (message);
+    spp->sock_fd = g_unix_fd_list_get (fd_list, 0, &error);
+    g_assert_no_error (error);
 
-	printf ("handle_new_conn called for device: %s fd: %d!\n", device, spp->sock_fd);
+    printf ("handle_new_conn called for device: %s fd: %d!\n", device, spp->sock_fd);
 
-	if (getsockname (spp->sock_fd, (struct sockaddr *) &(spp->local), &optlen) < 0) {
-		printf("handle_new_conn: local getsockname failed: %s\n", strerror(errno));
-		return FALSE;
-	}
+    if (getsockname (spp->sock_fd, (struct sockaddr *) &(spp->local), &optlen) < 0) {
+        printf("handle_new_conn: local getsockname failed: %s\n", strerror(errno));
+        return FALSE;
+    }
 
-	print_bdaddr("handle_new_conn local: ", &(spp->local.rc_bdaddr));
+    print_bdaddr("handle_new_conn local: ", &(spp->local.rc_bdaddr));
 
-	if (getpeername (spp->sock_fd, (struct sockaddr *) &(spp->remote), &optlen) < 0) {
-		printf("handle_new_conn: remote getsockname failed: %s\n", strerror(errno));
-		return FALSE;
-	}
+    if (getpeername (spp->sock_fd, (struct sockaddr *) &(spp->remote), &optlen) < 0) {
+        printf("handle_new_conn: remote getsockname failed: %s\n", strerror(errno));
+        return FALSE;
+    }
 
-	print_bdaddr("handle_new_conn remote: ", &(spp->remote.rc_bdaddr));
-	
-	// finished with method call; no reply sent
-	g_dbus_method_invocation_return_value(invocation, NULL);
+    print_bdaddr("handle_new_conn remote: ", &(spp->remote.rc_bdaddr));
 
-	g_idle_add(server_read_data, spp);
-		
-	return TRUE;
+    // finished with method call; no reply sent
+    g_dbus_method_invocation_return_value(invocation, NULL);
+
+    g_idle_add(server_read_data, spp);
+
+    return TRUE;
 }
 
 int main(int argc, char *argv[])
 {
-	GDBusProxy *proxy;
-	GDBusConnection *conn;
-	GError *error = NULL;
-	OrgBluezProfile1 *interface;
-	struct spp_data *spp;	
+    GDBusProxy *proxy;
+    GDBusConnection *conn;
+    GError *error = NULL;
+    OrgBluezProfile1 *interface;
+    struct spp_data *spp;
 
-	spp = g_new0 (struct spp_data, 1);
+    spp = g_new0 (struct spp_data, 1);
 
-	spp->loop = g_main_loop_new (NULL, FALSE);
-	
-	conn = g_bus_get_sync (G_BUS_TYPE_SYSTEM, NULL, &error);
-	g_assert_no_error (error);
+    spp->loop = g_main_loop_new (NULL, FALSE);
 
-	proxy = g_dbus_proxy_new_sync (conn,
-				G_DBUS_PROXY_FLAGS_NONE,
-				NULL,/* GDBusInterfaceInfo */
-				"org.bluez",/* name */
-				"/org/bluez",/* object path */
-				"org.bluez.ProfileManager1",/* interface */
-				NULL,/* GCancellable */
-				&error);
-	g_assert_no_error (error);
+    conn = g_bus_get_sync (G_BUS_TYPE_SYSTEM, NULL, &error);
+    g_assert_no_error (error);
 
-	if (register_profile (spp, proxy)) {
-		return 0;
-	}
+    proxy = g_dbus_proxy_new_sync (conn,
+                G_DBUS_PROXY_FLAGS_NONE,
+                NULL,/* GDBusInterfaceInfo */
+                "org.bluez",/* name */
+                "/org/bluez",/* object path */
+                "org.bluez.ProfileManager1",/* interface */
+                NULL,/* GCancellable */
+                &error);
+    g_assert_no_error (error);
 
-	interface = org_bluez_profile1_skeleton_new ();
+    if (register_profile (spp, proxy)) {
+        return 0;
+    }
 
-	g_signal_connect (interface,
-			"handle_new_connection",
-			G_CALLBACK (on_handle_new_connection),
-			spp);
+    interface = org_bluez_profile1_skeleton_new ();
 
-	error = NULL;
-	if (!g_dbus_interface_skeleton_export (G_DBUS_INTERFACE_SKELETON (interface),
-						conn,
-						"/bluetooth/profile/serial_port",
-						&error))
-	{
-		printf ("dbus_interface_skeleton_export failed for SPP!\n");
-		return 1;
-	}
+    g_signal_connect (interface,
+            "handle_new_connection",
+            G_CALLBACK (on_handle_new_connection),
+            spp);
 
-	g_main_loop_run (spp->loop);
+    error = NULL;
+    if (!g_dbus_interface_skeleton_export (G_DBUS_INTERFACE_SKELETON (interface),
+                        conn,
+                        "/bluetooth/profile/serial_port",
+                        &error))
+    {
+        printf ("dbus_interface_skeleton_export failed for SPP!\n");
+        return 1;
+    }
 
-	g_object_unref (proxy);
-	g_object_unref (conn);
+    g_main_loop_run (spp->loop);
 
-	return 0;
+    g_object_unref (proxy);
+    g_object_unref (conn);
+
+    return 0;
 }
